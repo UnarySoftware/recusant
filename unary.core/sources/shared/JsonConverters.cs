6655c8b42e0251e1917243d1f130a744cba1fccc
Withaust <withaust@gmail.com>
2026-04-29 13:46:29 +0300

    Structure folders in addons & Add additional Json converters

--- unary.core/sources/shared/JsonConverters.cs
+++ unary.core/sources/shared/JsonConverters.cs
@@ -9,7 +9,22 @@ namespace Unary.Core
         public static List<JsonConverter> Value { get; } =
         [
             new GodotJson<Variant>(),
+            new GodotJson<Vector2>(),
+            new GodotJson<Vector2I>(),
+            new GodotJson<Rect2>(),
+            new GodotJson<Rect2I>(),
             new GodotJson<Vector3>(),
+            new GodotJson<Vector3I>(),
+            new GodotJson<Transform2D>(),
+            new GodotJson<Vector4>(),
+            new GodotJson<Vector4I>(),
+            new GodotJson<Plane>(),
+            new GodotJson<Quaternion>(),
+            new GodotJson<Aabb>(),
+            new GodotJson<Basis>(),
+            new GodotJson<Transform3D>(),
+            new GodotJson<Projection>(),
+            new GodotJson<Color>(),
         ];
     }
 }
